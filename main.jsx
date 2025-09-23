63b2b52bd4f4df8f9160931362860e77918236dd
Bayfront302 <barcoos302@gmail.com>
2025-09-22 23:15:42 -0500

    Delete main.jsx

--- main.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Site from "../index.tsx"; // uses your existing canvas code file at repo root
-
-const rootEl = document.getElementById("root");
-const root = createRoot(rootEl);
-root.render(<Site />);
