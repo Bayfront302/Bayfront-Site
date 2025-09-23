3e919cf08fe906b7c4cf50d2fb036354099626d7
Bayfront302 <barcoos302@gmail.com>
2025-09-23 13:40:21 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import Site from "../index.tsx"; // this is your canvas file at the repo root
+
 
 const root = createRoot(document.getElementById("root"));
 root.render(<Site />);
